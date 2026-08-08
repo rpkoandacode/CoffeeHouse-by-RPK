bed38cf0e1b91234f16c967a6b8427709f8da3ea
Richard Pratama Koanda <rpkoanda@gmail.com>
2026-08-08 21:42:39 +0800

    nav bar created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+
 export default function Home() {
   return (
     <div>
-      <h1>Coffee House</h1>
-      <p>Brew Happiness, One Cup at a Time.</p>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
